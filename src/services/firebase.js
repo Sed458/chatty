4a33e94efe780bc6fe97c3b214890569fabc9f7b
Sed458 <sed458@nyu.edu>
2020-07-17 23:13:43 -0400

    app setup and Signup component created

---
+++ src/services/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAp_5C0x7rTGSYDogb7dmWk0ixN7gq4iqk",
+    authDomain: "chatty-415da.firebaseapp.com",
+    databaseURL: "https://chatty-415da.firebaseio.com"
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth;
+export const db = firebase.database();
